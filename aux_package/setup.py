45f1440f5cea44b51ddcbe5033a5123cf6a2c045
Everlasting-AM <warrserhouse@gmail.com>
2023-03-08 12:50:40 +0100

    Corrección organización ficheros aux

--- aux_package/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(name='aux_libraries',
-      version='0.1',
-      description='Funciones auxiliares para gestión de argumentos',
-      packages=['aux'],
-      author_email='am.guerreroserna@gmail.com',
-      zip_safe=False)
